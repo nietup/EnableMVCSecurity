3e5aaf38f1ba9517a3f6f7ba7981e33746cabd98
Jakub Nietupski <jnie@netcompany.com>
2020-07-23 13:05:24 +0200

    fixed authorization

--- src/main/java/security/example/mvc/SecurityConfig.java
+++ src/main/java/security/example/mvc/SecurityConfig.java
@@ -6,23 +6,37 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        Pbkdf2PasswordEncoder encoder = new Pbkdf2PasswordEncoder(); //password encoder must be defined, otherwise you'll get NPE during login
+
         auth.inMemoryAuthentication()
-                .withUser("user").password("user").roles("USER").and()
-                .withUser("admin").password("admin").roles("USER", "ADMIN");
+                .withUser("user")
+                .password(encoder.encode("user"))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password(encoder.encode("admin"))
+                .roles("USER", "ADMIN")
+        .and()
+        .passwordEncoder(encoder);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                .formLogin() //for user friendly login-page
+                .and()
+                .httpBasic() //for machines to login with basic authentication
+                .and()
                 .authorizeRequests()
-                .antMatchers("/gold").authenticated()
-                .antMatchers(HttpMethod.POST, "/deposit").authenticated()
+                .regexMatchers("/gold.*").authenticated() //remember to match paths with- and without the final "/"!
+                .regexMatchers(HttpMethod.POST, "/deposit.*").authenticated()
                 .anyRequest().permitAll();
     }
 }
