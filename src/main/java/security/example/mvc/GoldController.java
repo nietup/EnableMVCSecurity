40f047172016c82e89f742997a8b0264fe817f17
Jakub Nietupski <jnie@netcompany.com>
2020-07-23 12:21:47 +0200

    add controllers

--- src/main/java/security/example/mvc/GoldController.java
+++ src/main/java/security/example/mvc/GoldController.java
@@ -2,12 +2,28 @@ package security.example.mvc;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GoldController {
     @GetMapping("/gold")
     public ResponseEntity<String> getGold() {
-        return ResponseEntity.ok("GOLD 1000");
+        return ResponseEntity.ok("\uD83D\uDCB0 \uD83D\uDCB0 \uD83D\uDCB0 \uD83D\uDCB0 \uD83D\uDCB0 \uD83D\uDCB0\uD83D\uDCB0 \uD83D\uDCB0");
+    }
+
+    @GetMapping("/charity")
+    public ResponseEntity<String> getCharity() {
+        return ResponseEntity.ok("\uD83D\uDCB0");
+    }
+
+    @PostMapping("/deposit")
+    public ResponseEntity<String> depositFunds(String amount) {
+        return ResponseEntity.ok("Your deposit has been recorded.");
+    }
+
+    @PostMapping("/donation")
+    public ResponseEntity<String> donateFunds(String amount) {
+        return ResponseEntity.ok("Thank you for your generosity!");
     }
 }
